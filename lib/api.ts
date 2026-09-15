b5ed20638d0d7be16877779c5765a8a289e46480
SSEMWOGERERE948 <strevor948@gmail.com>
2026-09-15 17:12:47 +0300

    replaced with API_base_url

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = normalizeBaseUrl(process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://noblifi.ew.r.appspot.com");
-export const API_BASE_URL = normalizeBaseUrl(process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://127.0.0.1:8080");
+export const API_BASE_URL = normalizeBaseUrl(process.env.API_BASE_URL ?? "https://169-58-83-26.sslip.io");
+// export const API_BASE_URL = normalizeBaseUrl(process.env.API_BASE_URL ?? "http://127.0.0.1:8080");
 
 type FetchOptions = RequestInit & {
   fallback?: unknown;
